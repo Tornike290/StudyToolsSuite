3690a6e1609a801ad5911dd3d50f022003fd2722
AMISK9 <vgm210@aubg.edu>
2025-02-20 23:03:55 +0200

    initial commit

---
+++ StudyToolsSuite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudyToolsSuite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
